7d6408468206891050f567bbcc91afe3d6b4016d
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-08-30 12:40:06 -0700

    created function to filter reviews by date

--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ const NUM_REVIEWS = 20;
 
 
 
+
 axios.get(URI + SORT_NEW)
 .then(response => processAndFormatData(response.data))
-.then(reviews => console.log(reviews));
+.then(reviews => console.log(reviews, filterReviews(reviews, 30)));
 
 
 // Process data returned in response.data from axios GET
@@ -41,4 +42,40 @@ function processAndFormatData(html) {
   })
 
   return reviews;
+}
+
+// Given a list of reviews, returns a new list of reviews between today and number of days prior
+// If no number of days is supplied then will return the revies posted today
+// @param reviews the list of reviews [{date, rating}, .., ..]
+// @param numDays the number of days to include (e.g 7 - between today and 1 week ago)
+// @return list of filtered arrays
+function filterReviews(reviews, numDays = 0) {
+  const today = new Date();
+
+  if (numDays === 0) {
+    return reviews.filter(review => {
+      let reviewDate = review.date;
+      return dateIsEqual(today, reviewDate);
+    })
+  }
+  else {
+    const pastDay = new Date();
+    pastDay.setDate(today.getDate() - numDays);
+
+    return reviews.filter(review => {
+      let reviewDate = review.date;
+      return (reviewDate < today && reviewDate > pastDay)
+    })
+  }
+}
+
+// Given two date objects
+// @return true if they are the same day/month/year
+// @return false otherwise
+function dateIsEqual(date1, date2) {
+  return (
+    date1.getDate() === date2.getDate() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getFullYear() === date2.getFullYear()
+  )
 }
